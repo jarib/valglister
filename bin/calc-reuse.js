3f5d0364e8e2b04882ad5ddf89b4f5e3e1060fa5
Jari Bakken <jari.bakken@gmail.com>
2019-01-21 11:03:57 +0100

    reuse

--- bin/calc-reuse.js
+++ bin/calc-reuse.js
@@ -1,6 +1,6 @@
 const parse = require('csv-parse/lib/sync');
 const fs = require('fs-extra');
-const { groupBy, mapValues, values, keyBy } = require('lodash');
+const { groupBy, mapValues, values, keyBy, round } = require('lodash');
 
 (async () => {
     try {
@@ -8,28 +8,30 @@ const { groupBy, mapValues, values, keyBy } = require('lodash');
             columns: true,
         });
 
-        console.log(data[0]);
-
         const byKey = mapValues(
             groupBy(data, d => `${d.year}/${d.election}`),
             values =>
                 keyBy(
-                    values.filter(d => +d.cancidateId < 5),
+                    values.filter(d => +d.candidateId < 5),
                     d => `${d.name}/${d.yearBorn}`
                 )
         );
 
         let alsoInPrev = 0;
+        const keys = Object.keys(byKey['2015/kommunestyre']);
 
-        Object.keys(byKey['2015/kommunestyre']).forEach(key => {
+        keys.forEach(key => {
             if (byKey['2011/kommunestyre'][key]) {
                 alsoInPrev++;
             }
         });
 
+        const total = keys.length;
+
         console.log({
             alsoInPrev,
-            total: Object.keys(byKey['2015/kommunestyre']).length,
+            total,
+            percent: round((alsoInPrev * 100) / total),
         });
     } catch (error) {
         console.log(error);
